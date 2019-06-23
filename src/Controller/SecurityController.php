3d1e9534068d9163ab78a40a2493e739ac01ca45
Naoki Ikeguchi <siketyan@sikeserver.com>
2019-06-23 21:38:06 +0900

    Replace login form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,13 +2,27 @@
 
 namespace App\Controller;
 
+use App\Form\LoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    private $csrfTokenManager;
+
+    /**
+     * SecurityController constructor.
+     *
+     * @param CsrfTokenManagerInterface $csrfTokenManager
+     */
+    public function __construct(CsrfTokenManagerInterface $csrfTokenManager)
+    {
+        $this->csrfTokenManager = $csrfTokenManager;
+    }
+
     /**
      * @Route("/login", name="app_login")
      *
@@ -22,9 +36,16 @@ class SecurityController extends AbstractController
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
+        // CSRF token
+        $token = $this->csrfTokenManager->getToken('authenticate')->getValue();
+
+        $form = $this->createForm(LoginType::class, [
+            'username' => $lastUsername,
+            '_csrf_token' => $token,
+        ]);
 
         return $this->render('security/login.html.twig', [
-            'last_username' => $lastUsername,
+            'form' => $form->createView(),
             'error' => $error,
         ]);
     }
---
+++ src/Form/LoginType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LoginType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class)
+            ->add('password', PasswordType::class)
+            ->add('_csrf_token', HiddenType::class)
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'csrf_protection' => false,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return '';
+    }
+}
