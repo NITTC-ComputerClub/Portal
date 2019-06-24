c3e940eaa8228ddd962a9c3a94816a1aea55f8cb
Naoki Ikeguchi <siketyan@sikeserver.com>
2019-06-24 17:55:38 +0900

    Require ROLE_USER on UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,10 +3,19 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class UserController
+ *
+ * @Route("/user")
+ * @IsGranted("ROLE_USER")
+ *
+ * @package App\Controller
+ */
 class UserController extends AbstractController
 {
     private $repository;
@@ -21,7 +30,7 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/user/{id}", name="user")
+     * @Route("/{id}", name="user")
      *
      * @param int $id
      *
