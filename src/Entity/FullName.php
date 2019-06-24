b746d2555600b7f82c827b11024e875413e895b8
Naoki Ikeguchi <siketyan@sikeserver.com>
2019-06-24 16:48:57 +0900

    Add FullName entity and FullNameRepository by `make:entity`

---
+++ src/Entity/FullName.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\FullNameRepository")
+ */
+class FullName
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstNameKana;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastNameKana;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getFirstNameKana(): ?string
+    {
+        return $this->firstNameKana;
+    }
+
+    public function setFirstNameKana(string $firstNameKana): self
+    {
+        $this->firstNameKana = $firstNameKana;
+
+        return $this;
+    }
+
+    public function getLastNameKana(): ?string
+    {
+        return $this->lastNameKana;
+    }
+
+    public function setLastNameKana(string $lastNameKana): self
+    {
+        $this->lastNameKana = $lastNameKana;
+
+        return $this;
+    }
+}
