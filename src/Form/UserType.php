5cf76974d040fc53a964f0cb62319f71492dc0e4
Naoki Ikeguchi <siketyan@sikeserver.com>
2019-06-26 17:31:03 +0900

    Add padding to nested type

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -21,7 +21,11 @@ class UserType extends AbstractType
         $builder
             ->add('username', TextType::class)
             ->add('password', PasswordType::class)
-            ->add('name', NameType::class)
+            ->add('name', NameType::class, [
+                'attr' => [
+                    'class' => 'pl-md-5 pl-3'
+                ]
+            ])
             ->add('studentId', NumberType::class)
             ->add('birthday', DateType::class, [
                 'years' => range($currentYear - 100, $currentYear),
