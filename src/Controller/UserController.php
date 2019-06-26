939415a7f517e0bf9964b1fd142681566047b9da
Naoki Ikeguchi <siketyan@sikeserver.com>
2019-06-26 17:08:19 +0900

    Add UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,11 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
 use App\Repository\UserRepository;
+use Exception;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * Class UserController
@@ -18,15 +23,65 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class UserController extends AbstractController
 {
+    /**
+     * @var UserRepository
+     */
     private $repository;
 
     /**
-     * UserController constructor.
-     * @param UserRepository $repository
+     * @var UserPasswordEncoderInterface
      */
-    public function __construct(UserRepository $repository)
+    private $passwordEncoder;
+
+    /**
+     * UserController constructor.
+     *
+     * @param UserRepository               $repository
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     */
+    public function __construct(
+        UserRepository $repository,
+        UserPasswordEncoderInterface $passwordEncoder
+    )
     {
         $this->repository = $repository;
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    /**
+     * @Route("/create")
+     *
+     * @param Request $request
+     *
+     * @return Response
+     *
+     * @throws Exception
+     */
+    public function create(Request $request): Response
+    {
+        $form = $this->createForm(UserType::class, new User());
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var User $user */
+            $user = $form->getData();
+            $user->setPassword(
+                $this->passwordEncoder->encodePassword(
+                    $user,
+                    $user->getPassword()
+                )
+            );
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'The user was created successfully!');
+        }
+
+        return $this->render('user/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
     /**
