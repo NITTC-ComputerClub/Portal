b0d5c0a7aaaaf76060a2b8247a8463c0bc643ab5
Naoki Ikeguchi <siketyan@sikeserver.com>
2019-07-01 19:14:29 +0900

    Assert and get name data from NameTypeTest

--- tests/Form/UserTypeTest.php
+++ tests/Form/UserTypeTest.php
@@ -15,10 +15,6 @@ class UserTypeTest extends TypeTestCase
 {
     private const USERNAME = 'dummy_username';
     private const PASSWORD = 'dummy_password';
-    private const NAME_FIRST = 'dummy_first_name';
-    private const NAME_LAST = 'dummy_last_name';
-    private const NAME_FIRST_KANA = 'dummy_first_name_kana';
-    private const NAME_LAST_KANA = 'dummy_last_name_kana';
     private const STUDENT_ID = 12345;
 
     /**
@@ -26,7 +22,9 @@ class UserTypeTest extends TypeTestCase
      */
     private $form;
 
-    /** @var DateTimeInterface The date to test with. */
+    /**
+     * @var DateTimeInterface The date to test with.
+     */
     private $today;
 
     /**
@@ -49,12 +47,7 @@ class UserTypeTest extends TypeTestCase
         $form->submit([
             'username' => self::USERNAME,
             'password' => self::PASSWORD,
-            'name' => [
-                'firstName' => self::NAME_FIRST,
-                'lastName' => self::NAME_LAST,
-                'firstNameKana' => self::NAME_FIRST_KANA,
-                'lastNameKana' => self::NAME_LAST_KANA,
-            ],
+            'name' => NameTypeTest::getNameData(),
             'studentId' => self::STUDENT_ID,
             'birthday' => [
                 'year' => $this->today->format('Y'),
@@ -74,11 +67,9 @@ class UserTypeTest extends TypeTestCase
         $this->assertInstanceOf(User::class, $user);
         $this->assertEquals(self::USERNAME, $user->getUsername());
         $this->assertEquals(self::PASSWORD, $user->getPassword());
-        $this->assertEquals(self::NAME_FIRST, $user->getName()->getFirstName());
-        $this->assertEquals(self::NAME_LAST, $user->getName()->getLastName());
-        $this->assertEquals(self::NAME_FIRST_KANA, $user->getName()->getFirstNameKana());
-        $this->assertEquals(self::NAME_LAST_KANA, $user->getName()->getLastNameKana());
         $this->assertEquals(self::STUDENT_ID, $user->getStudentId());
         $this->assertEquals($this->today->format('Ymd'), $user->getBirthday()->format('Ymd'));
+
+        NameTypeTest::assertName($this, $user->getName());
     }
 }
