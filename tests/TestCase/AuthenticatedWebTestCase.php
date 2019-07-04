7d3dbd1c2dc824c8b4f7d1cac8946fc4bead0113
Naoki Ikeguchi <siketyan@sikeserver.com>
2019-07-04 18:02:25 +0900

    Add AuthenticatedWebTestCase

---
+++ tests/TestCase/AuthenticatedWebTestCase.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\TestCase;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\Common\Persistence\AbstractManagerRegistry;
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Client;
+
+class AuthenticatedWebTestCase extends WebTestCase
+{
+    private $FIREWALL_NAME = 'main';
+
+    private function getDoctrine(): AbstractManagerRegistry
+    {
+        return parent::getContainer()->get('doctrine');
+    }
+
+    protected function getUserRepository(): UserRepository
+    {
+        return $this->getDoctrine()->getRepository(User::class);
+    }
+
+    protected function getTestUser(): User
+    {
+        return $this->getUserRepository()->find(1);
+    }
+
+    private function login(): void
+    {
+        $this->loginAs(
+            $this->getTestUser(),
+            $this->FIREWALL_NAME
+        );
+    }
+
+    protected function createAuthenticatedClient(): Client
+    {
+        $this->login();
+
+        return $this->makeClient();
+    }
+}
